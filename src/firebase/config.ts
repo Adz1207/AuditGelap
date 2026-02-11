82113998377660777bad2efb47034d2bb2ac507a
Bugi Art's <BugieCoituz888@gmail.com>
2026-02-11 09:05:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5361134852-a6045",
+  "appId": "1:520792288801:web:defb5a7edd9eaefbda3d60",
+  "apiKey": "AIzaSyDUHWpy6mnuHwjywjYa--f5ZBpc6Bn6O5c",
+  "authDomain": "studio-5361134852-a6045.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "520792288801"
+};
