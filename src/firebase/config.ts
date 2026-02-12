0c7d1bdf326d5395491baba5ed48cf17f6f68f92
Bugi Art's <BugieCoituz888@gmail.com>
2026-02-12 07:10:13 +0000

    echo "REACT_APP_GOOGLE_API_KEY=AIzaSyBZI-a3n9t1gldCOyrM1-CbPAaPmSXpx3Y"

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-5361134852-a6045",
   "appId": "1:520792288801:web:defb5a7edd9eaefbda3d60",
-  "apiKey": process.env.REACT_APP_GOOGLE_API_KEY,
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   "authDomain": "studio-5361134852-a6045.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "520792288801"
