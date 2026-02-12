dbc74eec5661f87e0423f846a70080b7ad400e05
Bugi Art's <BugieCoituz888@gmail.com>
2026-02-12 07:00:31 +0000

    SECURITY: Revoke leaked API key and migrate to env variables

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-5361134852-a6045",
   "appId": "1:520792288801:web:defb5a7edd9eaefbda3d60",
-  "apiKey": "AIzaSyDUHWpy6mnuHwjywjYa--f5ZBpc6Bn6O5c",
+  "apiKey": process.env.REACT_APP_GOOGLE_API_KEY,
   "authDomain": "studio-5361134852-a6045.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "520792288801"
